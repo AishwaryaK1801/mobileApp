2fe450900b79ce0b3337517f40205616d017f01a
aishu1801 <kulkarniaishwarya121@gmail.com>
2024-05-29 18:51:51 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MobileComponent } from './shared/components/mobile/mobile.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MobileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
